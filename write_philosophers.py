82d40e6e11af7191c5f677ce539ee29a50a91c05
JoeByrne1 <Joe_Byrne1@baylor.edu>
2024-09-30 11:18:00 -0500

    print1

--- write_philosophers.py
+++ write_philosophers.py
@@ -1,15 +1,1 @@
-def main():
-    #open a file named philosophers.txt
-    outfile = 
-
-
-    #write the names of three philosophers to the file
-    #John Locke, David Hume and Edmund Burke
-
-
-
-    #close the file
-
-
-#call the main function
-main()
+print(1)
